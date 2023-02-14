6e0c41bfcc9107d73525fbde0913762ea4e8c7fa
Sam Quist <quistsa@mail.gvsu.edu>
2023-02-14 15:31:23 -0500

    Initial setup

---
+++ index.js
@@ -0,0 +1,44 @@
+//index.js verifies authentication and handles web requests to redirect users to the different pages
+//Sam Quist, Up In The Cloud
+
+const express = require('express');
+const session = require('express-session');
+
+const UserController = require('./userController');
+const userController = new UserController();
+
+const LoginController = require('./loginController');
+const loginController = new LoginController();
+
+const bodyParser = require('body-parser');
+const { response } = require('express');
+
+//start server
+const app = express();
+const port = 3000;
+
+function isAuthenticated(req, res, next) {
+
+    console.log('Enter isAuthenticated')
+    console.log(req.session)
+    if (req.session.user) {
+        console.log("Already logged in :)")
+        next()
+    } else {
+        console.log("redirecting to login page")
+        res.redirect('/login')
+    }
+}
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/login', (req, res) => {
+    loginController.loginPage(req, res)
+})
+
+app.post('/login', (req, res) => {
+    loginController.requestLogin(req, res)
+})
---
+++ loginController.js
@@ -0,0 +1,31 @@
+
+class LoginController {
+
+    loginPage(req, res) {
+        res.render('login', { message: 'Please login to view your cars' })
+    }
+
+    requestLogin(req, res, next) {
+
+        console.log("Request body")
+        console.log(req.body)
+        //check database for userIDs
+        //verify that user input matches
+            console.log("Creating new session");
+            req.session.regenerate((err) => {
+                if (err) next(err)
+                req.session.user = req.body.username;
+                console.log('here!');
+                res.redirect('/admin'); //only if user is admin, otherwise /faculty
+            })
+        }
+    
+    logout(req, res) {
+        req.session.destroy(function(){
+            res.redirect('/login');
+          });
+    }
+}
+
+module.exports = LoginController
+
