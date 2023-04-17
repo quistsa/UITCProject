d8631549c41d40602b97b177f1aadfc39cd3c5c1
Sam Quist <quistsa@mail.gvsu.edu>
2023-04-17 14:08:52 -0400

    minor fix

--- userController.js
+++ userController.js
@@ -80,11 +80,6 @@ class UserController{
             testUser.id = user.id;
             res.render('faculty/facultyForm', { user: testUser });
             return;
-        } else if (userDB.findUser(testUser.userID) != null){
-            testUser.id = user.id;
-            testUser.errors.push("User with ID of " + testUser.userID + " already exists.")
-            res.render('faculty/facultyForm', { user: testUser });
-            return;
         }
 
         if (user == null) {
